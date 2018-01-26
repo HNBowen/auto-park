3cc96349204888236708492d097cac62341cf5b3
Hank <hbowen1123@gmail.com>
2018-01-25 18:53:23 -0600

    found way to execute predefined JS with executeScript

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var webdriver = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome')
 var nodemailer = require('nodemailer');
+var $ = require('jquery');
+
 var config = require('./.config');
 
 //helper function to find value from drop downs
@@ -11,7 +13,26 @@ function findVal(options, selection) {
            val = options[i].value;
         }
     }
+    console.log(val);
     return val;
+}
+
+function findElementById(id) {
+  return browser.findElements(webdriver.By.css('[id='+id+']')).then(function(result) {
+    return result[0];
+  })
+}
+function selectProperty() {
+  let $parkedAtCommunityDropDown = $("#parkedAtCommunityDropDown");
+  let options = $parkedAtCommunityDropDown.find("option");
+  let value = findVal(options, config.property)
+
+  $parkedAtCommunityDropDown.val(value);
+  $parkedAtCommunityDropDown.trigger("change");
+}
+
+function sayHi() {
+  console.log("hi")
 }
 
 //initialize the web driver browser
@@ -19,13 +40,53 @@ var browser = new webdriver.Builder()
                            .withCapabilities(webdriver.Capabilities.chrome())
                            .build();
 
-//navigate to parkspeedy.com
-browser.get("http://www.parkspeedy.com").then(()=> {
-  console.log("poop smear");
-})
+// //navigate to parkspeedy.com
+// browser.get("http://www.parkspeedy.com")
+// //find the #createNewPassButton and click
+// browser.wait(findElementById("createNewPassButton"), 100000).then(() => {
+//   browser.findElement(webdriver.By.id("createNewPassButton")).click();
+//   browser.wait(findElementById("parkedAtCommunityDropDown"), 100000000).then(() => {
+//     browser.executeScript(async (selectProperty, findVal, config) => {
+//       window.config = config;
+//       //append functions as scripts
+//       var selectPropertyScript = document.createElement("script")
+//       var findValScript = document.createElement("script")
 
-//find the #createNewPassButton
-//click
+//       selectPropertyScript.innerHTML = selectProperty;
+//       findValScript.innerHTML = findVal;
+
+//       await document.head.appendChild(selectPropertyScript);
+//       await document.head.appendChild(findValScript);
+
+//       selectProperty();
+//     }, selectProperty, findVal, config)
+//   })
+// })
+
+const run = async () => {
+  await browser.get("http://www.parkspeedy.com");
+  await browser.findElement(webdriver.By.id("createNewPassButton")).click();
+
+  await browser.executeScript(async (selectProperty, findVal, config) => {
+    window.config = config;
+    //append functions as scripts
+    var selectPropertyScript = document.createElement("script")
+    var findValScript = document.createElement("script")
+
+    selectPropertyScript.innerHTML = selectProperty;
+    findValScript.innerHTML = findVal;
+
+    await document.head.appendChild(selectPropertyScript);
+    await document.head.appendChild(findValScript);
+  }, selectProperty, findVal, config)
+
+  await browser.executeScript(() => {
+    selectProperty();
+  })
+} 
+
+run();
+
 
 //wait for next page to load, then
   //find the property drop down #parkedAtCommunityDropDown
